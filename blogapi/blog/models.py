950c7a09488925fb2bdd5531cf70e67b83ed4c79
SherlockH0 <ddmlf7@gmail.com>
2024-10-24 16:39:04 +0300

    Add blog models

--- blogapi/blog/models.py
+++ blogapi/blog/models.py
@@ -1,3 +1,34 @@
+from datetime import timedelta
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    # For the AI auto-answer features
+    automaticly_answer_comments = models.BooleanField(default=True)
+    authomatic_answer_delay = models.DurationField(default=timedelta(minutes=30))
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        "Comment", on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    content = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    is_blocked = models.BooleanField(default=False)
+    is_AI_generated = models.BooleanField(default=False)
--- blogapi/project/settings/base.py
+++ blogapi/project/settings/base.py
@@ -12,6 +12,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blogapi.blog.apps.BlogConfig",
 ]
 
 MIDDLEWARE = [
