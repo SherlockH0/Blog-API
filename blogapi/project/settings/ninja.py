8f2f7797768de9b09145cf00753178924576fcc8
SherlockH0 <ddmlf7@gmail.com>
2024-10-29 17:35:49 +0200

    Move SIGNING_KEY from ninja.py to local settings

--- blogapi/project/settings/ninja.py
+++ blogapi/project/settings/ninja.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 NINJA_JWT = {
-    "SIGNING_KEY": "bmxex)1w=o^2**5tzj&uyam9c^y5)b!)@be+=(4yb63r!d$k)x",
+    "SIGNING_KEY": NotImplemented,
 }
 
 
--- blogapi/project/settings/templates/settings.unittest.py
+++ blogapi/project/settings/templates/settings.unittest.py
@@ -6,3 +6,7 @@ DATABASES = {
         "NAME": BASE_DIR.parent / "db.sqlite3",  # pyright: ignore
     }
 }
+
+NINJA_JWT = {
+    "SIGNING_KEY": "bmxex)1w=o^2**5tzj&uyam9c^y5)b!)@be+=(4yb63r!d$k)x",
+}
