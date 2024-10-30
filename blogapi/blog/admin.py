0e66a6a6a6c409cdae74956e9c04e16b8b4144ad
SherlockH0 <ddmlf7@gmail.com>
2024-10-30 12:37:18 +0200

    Replace blocked field with status field

--- blogapi/blog/admin.py
+++ blogapi/blog/admin.py
@@ -5,9 +5,9 @@ from blogapi.blog.models import Comment, Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["title", "author"]
+    list_display = ["title", "author", "status"]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ["author", "post", "parent_comment", "blocked", "ai_generated"]
+    list_display = ["author", "post", "parent_comment", "status", "ai_generated"]
--- blogapi/blog/models.py
+++ blogapi/blog/models.py
@@ -1,17 +1,38 @@
 from datetime import timedelta
 
 from django.conf import settings
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
+class Statuses(models.TextChoices):
+    ACTIVE = "+", "Active"
+    BLOCKED = "-", "Blocked"
+    IN_MODERATION = "/", "In moderation"
+
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     title = models.CharField(max_length=300)
-    content = models.TextField()
+    content = models.TextField(
+        validators=[
+            MinLengthValidator(300, "The post must be at least 300 characters.")
+        ]
+    )
 
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(  # pyright: ignore
+        max_length=1,
+        choices=Statuses,  # pyright: ignore
+        default=Statuses.IN_MODERATION,
+    )
 
     # For the AI auto-answer features
     automatically_answer_comments = models.BooleanField(default=True)
@@ -22,18 +43,35 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
     parent_comment = models.ForeignKey(
-        "Comment", on_delete=models.SET_NULL, null=True, blank=True
+        "Comment",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
     )
 
-    content = models.TextField()
+    content = models.TextField(
+        max_length=600,
+        validators=[MinLengthValidator(15, "Enter at least 15 characters.")],
+    )
 
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
-    blocked = models.BooleanField(default=False)
+    status = models.CharField(  # pyright: ignore
+        max_length=1,
+        choices=Statuses,  # pyright: ignore
+        default=Statuses.IN_MODERATION,
+    )
+
     ai_generated = models.BooleanField(default=False)
 
     def __str__(self):
