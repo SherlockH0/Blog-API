2bf6c8a19d4ee42ea192b45cb27fc91f166fce08
SherlockH0 <ddmlf7@gmail.com>
2024-11-01 18:53:53 +0200

    Add get user route

--- blogapi/users/api.py
+++ blogapi/users/api.py
@@ -1,14 +1,16 @@
 from typing import Self
 
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
-from ninja import Router, Schema
+from django.shortcuts import get_object_or_404
+from ninja import ModelSchema, Router, Schema
 from pydantic import field_validator, model_validator
 from pydantic_core import PydanticCustomError
 
 
-class UserSchema(Schema):
+class UserIn(Schema):
     username: str
     password: str
 
@@ -35,11 +37,22 @@ class UserSchema(Schema):
         return v
 
 
+class UserOut(Schema):
+    id: int
+    username: str
+
+
 router = Router()
 
 
 @router.post("")
-def create_user(request, payload: UserSchema):
+def create_user(request, payload: UserIn):
     user = User.objects.create_user(**payload.dict())
 
     return {"id": user.pk}
+
+
+@router.get("{user_id}/", response=UserOut)
+def get_user(request, user_id: int):
+    comment = get_object_or_404(get_user_model(), id=user_id)
+    return comment
