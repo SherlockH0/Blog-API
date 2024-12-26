3c6e08194cd34613655271aec3e3972a89ee46a1
SherlockH0 <ddmlf7@gmail.com>
2024-12-26 22:41:50 +0200

    Testing

--- blogapi/blog/api/posts.py
+++ blogapi/blog/api/posts.py
@@ -17,7 +17,7 @@ router = Router()
 @router.post("", auth=JWTAuth(), response={201: PostOut})
 def create_post(request, payload: PostIn):
     post = new_post(request.auth.id, payload.dict())
-    return 201, post
+    return 404, post
 
 
 # Read
